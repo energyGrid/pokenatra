b92c6f9e7ea829a8627c7293610c5e738776a37c
Scott Pierce <scottpiercew@gmail.com>
2017-05-30 21:31:54 -0400

    added create feature

--- app.rb
+++ app.rb
@@ -18,7 +18,16 @@ get '/pokemon' do
   erb :"pokemon/index"
 end
 
+get '/pokemon/new' do
+  erb :"pokemon/new"
+end
+
 get '/pokemon/:id' do
   @pokemon = Pokemon.find(params[:id])
   erb :"pokemon/show"
+end
+
+post '/pokemon' do
+  @pokemon = Pokemon.create(params[:pokemon])
+  redirect "/pokemon/#{@pokemon.id}"
 end
